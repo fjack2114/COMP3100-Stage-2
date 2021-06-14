1302857732399b5cd4d08ff4360003649d396850
Fred <frederick-lishia.jack@students.mq.edu.au>
2021-06-14 16:49:31 +1000

    Working full marks algorithm/other changes
    finished algorithm/adjusted parse server method/added terminate server method

--- src/Client.java
+++ src/Client.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Client {
 	private static Socket socket = null;
@@ -30,6 +31,7 @@ public class Client {
 	private int jobCores = 0;
 	private int jobMemory = 0;
 	private int jobDisk = 0;
+	private int numServers = 0;
 	private String jobID = EMPTYSTRING;
 
 	public static void main(String[] args) throws IOException {
@@ -71,20 +73,27 @@ public class Client {
 			outMessage(QUIT);
 		}
 
+		setServerInformation = XML.parse("ds-system.xml");
+
 		// Event loop to handle incoming jobs
 		while (!incomingMessage.contains(NONE)) {
+
 			if (incomingMessage.contains(JOBN)) {
 				String[] splitStr = incomingMessage.split(PARSEWHITESPACE);
 				jobID = splitStr[2];
-				outMessage(getsCapable(incomingMessage));
+				outMessage(getsAvailable(incomingMessage));
 			}
 			if (incomingMessage.contains(DATA)) {
 				String[] splitData = incomingMessage.split(PARSEWHITESPACE);
-				int numServers = Integer.parseInt(splitData[1]);
+				numServers = Integer.parseInt(splitData[1]);
 				outMessage(OK);
-				incomingMessage = inMessage();
-				String[] splitServers = incomingMessage.split(PARSEWHITESPACE);
-				parseServerInfo(numServers, splitServers);
+
+					for(int i = 0; i < numServers; i++) {
+						incomingMessage = inMessage();
+						String[] splitServers = incomingMessage.split(PARSEWHITESPACE);
+						parseServerInfo(numServers, splitServers);
+
+					}
 				outMessage(OK);
 			}
 			if (incomingMessage.contains(OK)) {
@@ -94,7 +103,13 @@ public class Client {
 				outMessage(REDY);
 			}
 			if (incomingMessage.contains(".")) {
-				outMessage(onlyFit(jobID, setServerInformation));
+				if(numServers > 0) {
+					outMessage(terminateServers());
+					outMessage(onlyFit(jobID, setServerInformation));
+				}
+				else if (numServers == 0){
+					outMessage(Objects.requireNonNull(processJob(jobID, setServerInformation)));
+				}
 				serverInformation.clear();
 			}
 			incomingMessage = inMessage();
@@ -133,27 +148,49 @@ public class Client {
 	}
 
 	private String onlyFit(String jobID, ArrayList<StaticServerList> staticServerInformation) {
-		StaticServerList server;
 		Server available = serverInformation.get(0);
-		for(int i = 0; i < staticServerInformation.size(); i++) {
-			if(jobCores == staticServerInformation.get(i).cores && staticServerInformation.get(i).memory > jobMemory) {
-				server = staticServerInformation.get(i);
-				return "SCHD " + jobID + WHITESPACE + server.serverType + WHITESPACE + 0 + NEWLINE;
+
+		for(int h = 0; h < serverInformation.size(); h++) {
+			if (serverInformation.get(h).cores >= jobCores  && serverInformation.get(h).cores - jobCores <= 7 && serverInformation.get(h).memory >= jobMemory && serverInformation.get(h).disk >= jobDisk) {
+				return "SCHD " + jobID + WHITESPACE + serverInformation.get(h).serverType + WHITESPACE + serverInformation.get(h).serverID + NEWLINE;
 			}
 		}
+		for (int i = 0; i < staticServerInformation.size(); i++) {
+			if (staticServerInformation.get(i).cores >= jobCores&& staticServerInformation.get(i).memory > jobMemory && staticServerInformation.get(i).disk > jobDisk) {
+				return "SCHD " + jobID + WHITESPACE + staticServerInformation.get(i).serverType + WHITESPACE + ((int) (Math.random() * (1 + 1)))+ NEWLINE;
+			}
+		}
+
 		return "SCHD " + jobID + WHITESPACE + available.serverType + WHITESPACE + available.serverID + NEWLINE;
 	}
 
-	private String getsCapable(String job) {
+	private String processJob(String jobID, ArrayList<StaticServerList> staticServerInformation) {
+		for (int i = 0; i < staticServerInformation.size(); i++) {
+			if (staticServerInformation.get(i).cores >= jobCores&& staticServerInformation.get(i).memory > jobMemory && staticServerInformation.get(i).disk > jobDisk) {
+				return "SCHD " + jobID + WHITESPACE + staticServerInformation.get(i).serverType + WHITESPACE + ((int) (Math.random() * (1 + 1))) + NEWLINE;
+			}
+		}
+		return null;
+	}
+
+	private String terminateServers() {
+		for(int i = 1; i < numServers; i++) {
+			if (serverInformation.get(i).status.equals("idle")) {
+				return "TERM " + serverInformation.get(i).serverType + WHITESPACE + serverInformation.get(i).serverID + NEWLINE;
+			}
+		}
+		return "";
+	}
+
+	private String getsAvailable(String job) {
 		String[] splitStr = job.split(PARSEWHITESPACE);
 		jobCores = Integer.parseInt(splitStr[4]);
 		jobMemory = Integer.parseInt(splitStr[5]);
 		jobDisk = Integer.parseInt(splitStr[6]);
-		return "GETS Capable " + jobCores + WHITESPACE + jobMemory + WHITESPACE + jobDisk + NEWLINE;
+		return "GETS Avail " + jobCores + WHITESPACE + jobMemory + WHITESPACE + jobDisk + NEWLINE;
 	}
 
 	private void parseServerInfo(int numServers, String[] splitServers) {
-		for(int i = 0; i < numServers; i++) {
 			String serverType = splitServers[0];
 			String serverID = splitServers[1];
 			String status = splitServers[2];
@@ -166,7 +203,6 @@ public class Client {
 
 			Server serverData = new Server(serverType, serverID, status, startTime, cores, memory, disk, jobsWaiting, jobsRunning);
 			serverInformation.add(serverData);
-		}
 	}
 	
 }
